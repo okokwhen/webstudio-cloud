8e15a9fa4c02fd6d05f79af7ac6fed928cce0b09
root <root@vmi2353003.contaboserver.net>
2025-02-02 07:47:03 -0600

    init repo

---
+++ app/__generated__/[auth]._index.tsx
@@ -0,0 +1,58 @@
+/* eslint-disable */
+      /* This is a auto generated file for building the project */ 
+
+
+      import { Fragment, useState } from "react";
+      import type { FontAsset, ImageAsset } from "@webstudio-is/sdk";
+      import { useResource, useVariableState } from "@webstudio-is/react-sdk/runtime";
+      import { Body as Body, RemixForm as RemixForm } from "@webstudio-is/sdk-components-react-router";
+import { Box as Box, Heading as Heading, Paragraph as Paragraph, HtmlEmbed as HtmlEmbed, Input as Input, Button as Button } from "@webstudio-is/sdk-components-react";
+
+
+      export const siteName = undefined;
+
+      export const favIconAsset: ImageAsset | undefined =
+        undefined;
+
+      // Font assets on current page (can be preloaded)
+      export const pageFontAssets: FontAsset[] =
+        []
+
+      export const pageBackgroundImageAssets: ImageAsset[] =
+        []
+
+      
+
+      
+
+      const Page = ({ }: { system: any; }) => {
+return <Body
+className={`w-body cs1bwfe cdtpo18 c17oxavp c1jbq3j1 c1ivsz46 c1iw9sd4 c1mo3ffr`}>
+<Box
+className={`w-box c1ojcter c7ownnw`}>
+<Heading
+className={`w-heading c1ttuzxs c1dmgvcw`}>
+{"Enter Your Phone Number"}
+</Heading>
+<Paragraph
+className={`w-paragraph c1w2n47q c1dmgvcw`}>
+{"We'll send you a one-time password (OTP) to verify your phone number"}
+</Paragraph>
+<HtmlEmbed
+className={`w-html-embed`} />
+</Box>
+<RemixForm
+className={`w-form`}>
+<Input
+className={`w-text-input`} />
+<Button
+className={`w-button`}>
+{"Button text you can edit"}
+</Button>
+</RemixForm>
+</Body>
+}
+
+
+      export { Page }
+    
---
+++ app/__generated__/_index.tsx
@@ -0,0 +1,369 @@
+/* eslint-disable */
+      /* This is a auto generated file for building the project */ 
+
+
+      import { Fragment, useState } from "react";
+      import type { FontAsset, ImageAsset } from "@webstudio-is/sdk";
+      import { useResource, useVariableState } from "@webstudio-is/react-sdk/runtime";
+      import { Body as Body, Link as Link } from "@webstudio-is/sdk-components-react-router";
+import { Box as Box, Image as Image, Button as Button, Text as Text, HtmlEmbed as HtmlEmbed, Paragraph as Paragraph, Bold as Bold, Heading as Heading } from "@webstudio-is/sdk-components-react";
+import { NavigationMenu as NavigationMenu, NavigationMenuList as NavigationMenuList, NavigationMenuItem as NavigationMenuItem, NavigationMenuTrigger as NavigationMenuTrigger, NavigationMenuContent as NavigationMenuContent, NavigationMenuLink as NavigationMenuLink, NavigationMenuViewport as NavigationMenuViewport, Dialog as Dialog, DialogTrigger as DialogTrigger, DialogOverlay as DialogOverlay, DialogContent as DialogContent, DialogTitle as DialogTitle, DialogDescription as DialogDescription, DialogClose as DialogClose } from "@webstudio-is/sdk-components-react-radix";
+
+
+      export const siteName = undefined;
+
+      export const favIconAsset: ImageAsset | undefined =
+        undefined;
+
+      // Font assets on current page (can be preloaded)
+      export const pageFontAssets: FontAsset[] =
+        []
+
+      export const pageBackgroundImageAssets: ImageAsset[] =
+        []
+
+      
+            
+
+            export const CustomCode = () => {
+              return (<></>);
+            }
+          
+
+      
+
+      const Page = ({ }: { system: any; }) => {
+return <Body
+className={`w-body cs1bwfe c1vbz5x0`}>
+<Box
+className={`w-box cu4mp27 c15sjw60 c1dtj5ge crrysu3 ccw0qbb cvzxz7j c11nb7ne`}>
+<Image
+src={"/assets/logo-typeface_67lHwrPVVIlxeZH3Ds_mP.jpg"}
+width={356}
+height={67}
+className={`w-image cy69cyc c1m0ktp9 c3z6yjq`} />
+<NavigationMenu
+className={`w-navigation-menu cgyr9oz c1b1yl8t cb61s8s`}>
+<NavigationMenuList
+className={`w-menu-list cu4mp27 c1ehgske cf77yik c97z1qr clipwj3 c1dtj5ge c1iw9sd4 c1e5380k crqyyz3 cw1fe1l c1po35wd`}>
+<NavigationMenuItem
+data-ws-index="0"
+className={`w-menu-item`}>
+<NavigationMenuTrigger>
+<Button
+className={`w-button c1d5yt3j c7fnkrg c1dtj5ge c1iw9sd4 cmthkik c1x7ee1t c1y4tju6 cq20zg9 clikaav c1hopcms c10r8lgl ctls8ky c71sr46 c18o3i2q c1qi22j9 c11yso45 cm91q73 c1jaqdov c885pcf c1efx6ce c1stjap9 c1ldecdk cy8mzru chb7a8k c1jxj627 c1mhv1rs c9phnef c14in0tx c12859n5 c1lazjhj`}>
+<Text
+className={`w-text`}>
+{"Resources"}
+</Text>
+<Box
+className={`w-box csbfeq7 ck8qwzt c1xvh1el c1htejvr c1g9uqnu cqi1ep8 cnzsjlt cdv2wof`}>
+<HtmlEmbed
+code={"<svg xmlns=\"http://www.w3.org/2000/svg\" viewBox=\"0 0 16 16\" fill=\"currentColor\" width=\"100%\" height=\"100%\" style=\"display: block;\"><path d=\"M4.04 6.284a.65.65 0 0 1 .92.001L8 9.335l3.04-3.05a.65.65 0 1 1 .921.918l-3.5 3.512a.65.65 0 0 1-.921 0L4.039 7.203a.65.65 0 0 1 .001-.92Z\"/></svg>"}
+className={`w-html-embed`} />
+</Box>
+</Button>
+</NavigationMenuTrigger>
+<NavigationMenuContent
+data-ws-index="0"
+className={`w-menu-content c1qfxbu6 c1dj3kbp c1bo75lt c7ownnw c1r47x89`}>
+<Box
+className={`w-box cu4mp27 c17f72d3 c12rlhi6 c1xysffg`}>
+<Box
+className={`w-box c1dlum0v c16n9p1a cmthkik c1x7ee1t c1y4tju6 cq20zg9 c1r47x89`}>
+{""}
+</Box>
+<Box
+className={`w-box ctahyjf cu4mp27 c17f72d3 c12rlhi6 coz4cqg`}>
+<NavigationMenuLink>
+<Link
+href={"https://ui.shadcn.com/docs/components/sheet"}
+className={`w-link cxdjzid cu4mp27 coz4cqg ctbssl9 c1e5380k crqyyz3 cmthkik c1x7ee1t c1y4tju6 cq20zg9 c18dg99t c87b9a ckd01ui c15bbt3u cplfgll cm2as2w chlh413 c33zlc8 c1wxk3fm`}>
+<Text
+className={`w-text clikaav c18dg99t c10r8lgl`}>
+{"Sheet"}
+</Text>
+<Paragraph
+className={`w-paragraph c11ubgxe c9l79tx cdgnnju c12507vo cznz35m clikaav c149h52m c1qdh9q7 cw1fe1l`}>
+{"Extends the Dialog component to display content that complements the main content of the screen."}
+</Paragraph>
+</Link>
+</NavigationMenuLink>
+<NavigationMenuLink>
+<Link
+href={"https://ui.shadcn.com/docs/components/navigation-menu"}
+className={`w-link cxdjzid cu4mp27 coz4cqg ctbssl9 c1e5380k crqyyz3 cmthkik c1x7ee1t c1y4tju6 cq20zg9 c18dg99t c87b9a ckd01ui c15bbt3u cplfgll cm2as2w chlh413 c33zlc8 c1wxk3fm`}>
+<Text
+className={`w-text clikaav c18dg99t c10r8lgl`}>
+{"Navigation Menu"}
+</Text>
+<Paragraph
+className={`w-paragraph c11ubgxe c9l79tx cdgnnju c12507vo cznz35m clikaav c149h52m c1qdh9q7 cw1fe1l`}>
+{"A collection of links for navigating websites."}
+</Paragraph>
+</Link>
+</NavigationMenuLink>
+<NavigationMenuLink>
+<Link
+href={"https://ui.shadcn.com/docs/components/tabs"}
+className={`w-link cxdjzid cu4mp27 coz4cqg ctbssl9 c1e5380k crqyyz3 cmthkik c1x7ee1t c1y4tju6 cq20zg9 c18dg99t c87b9a ckd01ui c15bbt3u cplfgll cm2as2w chlh413 c33zlc8 c1wxk3fm`}>
+<Text
+className={`w-text clikaav c18dg99t c10r8lgl`}>
+{"Tabs"}
+</Text>
+<Paragraph
+className={`w-paragraph c11ubgxe c9l79tx cdgnnju c12507vo cznz35m clikaav c149h52m c1qdh9q7 cw1fe1l`}>
+{"A set of layered sections of content—known as tab panels—that are displayed one at a time."}
+</Paragraph>
+</Link>
+</NavigationMenuLink>
+</Box>
+</Box>
+</NavigationMenuContent>
+</NavigationMenuItem>
+<NavigationMenuItem
+data-ws-index="1"
+className={`w-menu-item`}>
+<NavigationMenuTrigger>
+<Button
+className={`w-button c1d5yt3j c7fnkrg c1dtj5ge c1iw9sd4 cmthkik c1x7ee1t c1y4tju6 cq20zg9 clikaav c1hopcms c10r8lgl ctls8ky c71sr46 c18o3i2q c1qi22j9 c11yso45 cm91q73 c1jaqdov c885pcf c1efx6ce c1stjap9 c1ldecdk cy8mzru c1kauf7m c6rjz7g ce9ad6m cyvfrhb c16mkqgm c1lazjhj`}>
+<Text
+className={`w-text`}>
+{"Menu"}
+</Text>
+<Box
+className={`w-box csbfeq7 ck8qwzt c1xvh1el c1htejvr c1g9uqnu cqi1ep8 cnzsjlt cdv2wof`}>
+<HtmlEmbed
+code={"<svg xmlns=\"http://www.w3.org/2000/svg\" viewBox=\"0 0 16 16\" fill=\"currentColor\" width=\"100%\" height=\"100%\" style=\"display: block;\"><path d=\"M4.04 6.284a.65.65 0 0 1 .92.001L8 9.335l3.04-3.05a.65.65 0 1 1 .921.918l-3.5 3.512a.65.65 0 0 1-.921 0L4.039 7.203a.65.65 0 0 1 .001-.92Z\"/></svg>"}
+className={`w-html-embed`} />
+</Box>
+</Button>
+</NavigationMenuTrigger>
+<NavigationMenuContent
+data-ws-index="1"
+className={`w-menu-content c1qfxbu6 c1dj3kbp c1bo75lt c7ownnw c1r47x89`}>
+<Box
+className={`w-box cu4mp27 c17f72d3 c12rlhi6 c1po35wd`}>
+<Box
+className={`w-box ctahyjf cu4mp27 c17f72d3 c12rlhi6 coz4cqg`}>
+<NavigationMenuLink>
+<Link
+href={"https://ui.shadcn.com/docs/components/accordion"}
+className={`w-link cxdjzid cu4mp27 coz4cqg ctbssl9 c1e5380k crqyyz3 cmthkik c1x7ee1t c1y4tju6 cq20zg9 c18dg99t c87b9a ckd01ui c15bbt3u cplfgll cm2as2w chlh413 c33zlc8 c1wxk3fm`}>
+<Text
+className={`w-text clikaav c18dg99t c10r8lgl`}>
+{"Accordion"}
+</Text>
+<Paragraph
+className={`w-paragraph c11ubgxe c9l79tx cdgnnju c12507vo cznz35m clikaav c149h52m c1qdh9q7 cw1fe1l`}>
+{"A vertically stacked set of interactive headings that each reveal a section of content."}
+</Paragraph>
+</Link>
+</NavigationMenuLink>
+<NavigationMenuLink>
+<Link
+href={"https://ui.shadcn.com/docs/components/dialog"}
+className={`w-link cxdjzid cu4mp27 coz4cqg ctbssl9 c1e5380k crqyyz3 cmthkik c1x7ee1t c1y4tju6 cq20zg9 c18dg99t c87b9a ckd01ui c15bbt3u cplfgll cm2as2w chlh413 c33zlc8 c1wxk3fm`}>
+<Text
+className={`w-text clikaav c18dg99t c10r8lgl`}>
+{"Dialog"}
+</Text>
+<Paragraph
+className={`w-paragraph c11ubgxe c9l79tx cdgnnju c12507vo cznz35m clikaav c149h52m c1qdh9q7 cw1fe1l`}>
+{"A window overlaid on either the primary window or another dialog window, rendering the content underneath inert."}
+</Paragraph>
+</Link>
+</NavigationMenuLink>
+<NavigationMenuLink>
+<Link
+href={"https://ui.shadcn.com/docs/components/collapsible"}
+className={`w-link cxdjzid cu4mp27 coz4cqg ctbssl9 c1e5380k crqyyz3 cmthkik c1x7ee1t c1y4tju6 cq20zg9 c18dg99t c87b9a ckd01ui c15bbt3u cplfgll cm2as2w chlh413 c33zlc8 c1wxk3fm`}>
+<Text
+className={`w-text clikaav c18dg99t c10r8lgl`}>
+{"Collapsible"}
+</Text>
+<Paragraph
+className={`w-paragraph c11ubgxe c9l79tx cdgnnju c12507vo cznz35m clikaav c149h52m c1qdh9q7 cw1fe1l`}>
+{"An interactive component which expands/collapses a panel."}
+</Paragraph>
+</Link>
+</NavigationMenuLink>
+</Box>
+<Box
+className={`w-box ctahyjf cu4mp27 c17f72d3 c12rlhi6 coz4cqg`}>
+<NavigationMenuLink>
+<Link
+href={"https://ui.shadcn.com/docs/components/popover"}
+className={`w-link cxdjzid cu4mp27 coz4cqg ctbssl9 c1e5380k crqyyz3 cmthkik c1x7ee1t c1y4tju6 cq20zg9 c18dg99t c87b9a ckd01ui c15bbt3u cplfgll cm2as2w chlh413 c33zlc8 c1wxk3fm`}>
+<Text
+className={`w-text clikaav c18dg99t c10r8lgl`}>
+{"Popover"}
+</Text>
+<Paragraph
+className={`w-paragraph c11ubgxe c9l79tx cdgnnju c12507vo cznz35m clikaav c149h52m c1qdh9q7 cw1fe1l`}>
+{"Displays rich content in a portal, triggered by a button."}
+</Paragraph>
+</Link>
+</NavigationMenuLink>
+<NavigationMenuLink>
+<Link
+href={"https://ui.shadcn.com/docs/components/tooltip"}
+className={`w-link cxdjzid cu4mp27 coz4cqg ctbssl9 c1e5380k crqyyz3 cmthkik c1x7ee1t c1y4tju6 cq20zg9 c18dg99t c87b9a ckd01ui c15bbt3u cplfgll cm2as2w chlh413 c33zlc8 c1wxk3fm`}>
+<Text
+className={`w-text clikaav c18dg99t c10r8lgl`}>
+{"Tooltip"}
+</Text>
+<Paragraph
+className={`w-paragraph c11ubgxe c9l79tx cdgnnju c12507vo cznz35m clikaav c149h52m c1qdh9q7 cw1fe1l`}>
+{"A popup that displays information related to an element when the element receives keyboard focus or the mouse hovers over it."}
+</Paragraph>
+</Link>
+</NavigationMenuLink>
+<NavigationMenuLink>
+<Link
+href={"https://ui.shadcn.com/docs/components/button"}
+className={`w-link cxdjzid cu4mp27 coz4cqg ctbssl9 c1e5380k crqyyz3 cmthkik c1x7ee1t c1y4tju6 cq20zg9 c18dg99t c87b9a ckd01ui c15bbt3u cplfgll cm2as2w chlh413 c33zlc8 c1wxk3fm`}>
+<Text
+className={`w-text clikaav c18dg99t c10r8lgl`}>
+{"Button"}
+</Text>
+<Paragraph
+className={`w-paragraph c11ubgxe c9l79tx cdgnnju c12507vo cznz35m clikaav c149h52m c1qdh9q7 cw1fe1l`}>
+{"Displays a button or a component that looks like a button."}
+</Paragraph>
+</Link>
+</NavigationMenuLink>
+</Box>
+</Box>
+</NavigationMenuContent>
+</NavigationMenuItem>
+<NavigationMenuItem
+data-ws-index="2"
+className={`w-menu-item`}>
+<NavigationMenuLink>
+<Link
+href={"https://www.instagram.com/mplsexclusives/"}
+target={"_blank"}
+prefetch={"none"}
+className={`w-link c1d5yt3j c7fnkrg c1dtj5ge c1iw9sd4 cmthkik c1x7ee1t c1y4tju6 cq20zg9 clikaav c1hopcms c10r8lgl ctls8ky c71sr46 c18o3i2q c1qi22j9 c87b9a cm91q73 c1jaqdov c885pcf c1efx6ce c1stjap9 c1ldecdk cy8mzru c1kauf7m c14imdeu czol31a c9n0sll c1mkf0j4`}>
+{"instagram"}
+</Link>
+</NavigationMenuLink>
+</NavigationMenuItem>
+</NavigationMenuList>
+<Box
+className={`w-box c1bo75lt c19lvai3 c1o8swiu cu4mp27 c1iw9sd4 c1fn72dp`}>
+<NavigationMenuViewport
+className={`w-menu-viewport cgyr9oz c155sw9q c11ubgxe c9l79tx cmthkik c1x7ee1t c1y4tju6 cq20zg9 c13ztwrw c1siwi8o c14xn9zz c13kzqd7 c4s2irj c18roma6`} />
+</Box>
+</NavigationMenu>
+</Box>
+<Box
+tag={"section"}
+className={`w-box c1uz6xw7 c1nb0s53 cu4mp27 coz4cqg c1iw9sd4 c1dtj5ge cz0oo7p c6pyjvh ca5928l`}>
+<Image
+src={"/assets/logo_K8oPhIZdT21u2JNqOVJST.jpg"}
+width={1234}
+height={1155}
+className={`w-image cef0brg c1p1be02 c795p7h c1d46z1q cvmyoc ctbssl9`} />
+<Dialog>
+<DialogTrigger>
+<Button
+className={`w-button ctjochp c7fnkrg c1dtj5ge c1iw9sd4 c182wqiz cxz3q4v c155m8xy c13lvruk clikaav c1hopcms c10r8lgl ctls8ky c71sr46 c1boul6h c1kfa18p c1myxufp chfvogj c1b1yl8t crdep2e cmc8d9s c1jaqdov c885pcf c1efx6ce c1stjap9 c1ldecdk cinr0tx chlh413`}>
+{"Check our announcements"}
+</Button>
+</DialogTrigger>
+<DialogOverlay
+className={`w-dialog-overlay cmuvpwi c1qfxbu6 c1fn72dp c1dj3kbp c19wrn1i c1ewfgy3 csmh3uv c1daxzqz cu4mp27 c1p9hr4d c1mi2d6u c1q9b4p7 cqc7iq8`}>
+<DialogContent
+className={`w-dialog-content cv3kzx6 c1ewfgy3 cu4mp27 coz4cqg c17f72d3 c12rlhi6 c1qsigrr c1h713so cgyr9oz c182wqiz cxz3q4v c13lvruk c155m8xy cgad91 cvxm8iq c78u610`}>
+<Box
+className={`w-box cu4mp27 coz4cqg c1e5380k crqyyz3`}>
+<DialogTitle
+className={`w-dialog-title cguwokb ch95qu3 c1pdq8m0 cndm087 cttiol5 c1mo3ffr`}>
+{"Announcements"}
+</DialogTitle>
+<DialogDescription
+className={`w-dialog-description cguwokb ch95qu3 clikaav c1hopcms c1u1j6p9`}>
+<Text
+tag={"span"}
+className={`w-text`}>
+<Bold
+className={`w-bold-text`}>
+{"Last Updated: 12 / 2 / 2024"}
+</Bold>
+</Text>
+</DialogDescription>
+</Box>
+<Text
+className={`w-text c1mo3ffr`}>
+{"New Live Resin Cartridge flavors will be ready mid December, 10 new flavors. Just waiting on the packaging to arrive."}
+{""}
+<br />
+{""}
+{""}
+<br />
+{""}
+{"New Products:"}
+{""}
+<br />
+{""}
+{""}
+<br />
+{""}
+{"Exotic Flower"}
+{""}
+<br />
+{""}
+{""}
+<br />
+{""}
+{"A+ Indoors"}
+{""}
+<br />
+{""}
+{""}
+<br />
+{""}
+{"B+ Indoors"}
+{""}
+<br />
+{""}
+{""}
+<br />
+{""}
+{"Grape God Disposables"}
+{""}
+<br />
+{""}
+{""}
+<br />
+{""}
+{"Live Resin concentrates"}
+</Text>
+<DialogClose
+className={`w-close-button c1bo75lt c14mx6p cwbs124 ca5e57g cd40bcu c9i0pxj ckuwcyh cegh7e9 cu4mp27 c1dtj5ge c1iw9sd4 c1xvh1el c1htejvr c1d5yt3j ckd01ui c15bbt3u cm91q73 c7sfkvv c12jn520`}>
+<HtmlEmbed
+code={"<svg xmlns=\"http://www.w3.org/2000/svg\" viewBox=\"0 0 16 16\" fill=\"currentColor\" width=\"100%\" height=\"100%\" style=\"display: block;\"><path fill-rule=\"evenodd\" d=\"M13.566 2.434a.8.8 0 0 1 0 1.132L9.13 8l4.435 4.434a.8.8 0 0 1-1.132 1.132L8 9.13l-4.434 4.435a.8.8 0 0 1-1.132-1.132L6.87 8 2.434 3.566a.8.8 0 0 1 1.132-1.132L8 6.87l4.434-4.435a.8.8 0 0 1 1.132 0Z\" clip-rule=\"evenodd\"/></svg>"}
+className={`w-html-embed ccqflnv c4f7xok csfqdt0 c18di848 c1mo3ffr`} />
+</DialogClose>
+</DialogContent>
+</DialogOverlay>
+</Dialog>
+</Box>
+<Box
+className={`w-box ctjochp cu4mp27 coz4cqg c1iw9sd4 c1dtj5ge`}>
+<Box
+className={`w-box c12sgqm7 cv3kzx6`}>
+<Heading
+className={`w-heading c1qc765m cn5lumw c1dmgvcw c1wa3if2`}>
+{"Announcements"}
+</Heading>
+</Box>
+</Box>
+</Body>
+}
+
+
+      export { Page }
+    
